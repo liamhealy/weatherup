4fd78c744fbaa62c0189c7db898c761c6430f50d
Liam <liam.healy1@marist.edu>
2020-10-31 13:56:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">
+        <h1 id="Header-title"><u>Weatherup</u></h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
